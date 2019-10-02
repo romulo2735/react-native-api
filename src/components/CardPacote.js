468f2adda2096b6c9aed3e0947289a2182156d8a
Romulo <romulosousa27@hotmail.com>
2019-10-01 22:58:46 -0300

    Card pacote

---
+++ src/components/CardPacote.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
+
+export default class CardPacote extends Component {
+  render(){
+    const {onPress} = this.props;
+    const {id, nome, valor, dataInicio, dataFim} = this.state.detalhes;
+
+    return (
+        <TouchableWithoutFeedback onPress={ onPress }>
+          <View style={ styles.CardPacote }>
+            <Text>{ id }</Text>
+            <Text>{ nome }</Text>
+            <Text>{ valor }</Text>
+            <Text>{ dataInicio }</Text>
+            <Text>{ dataFim }</Text>
+          </View>
+        </TouchableWithoutFeedback>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  CardPacote: {
+    borderWidth: 2,
+    borderColor: 'red',
+  }
+});
