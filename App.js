bfd3a750b3909352ced800e9698ebe4473e1e58f
Romulo <romulosousa27@hotmail.com>
2019-10-01 23:18:52 -0300

    ajustando o namespace do app

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import ListaPacote from "./src/views/ListaPacote";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default class App extends Component {
+  render(){
+    return (
+        <ListaPacote />
+    );
+  }
+}
