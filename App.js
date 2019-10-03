976019f3923b6318c45adad0a7482363f622b3b0
Romulo <romulosousa27@hotmail.com>
2019-10-02 22:35:15 -0300

    listando os detalhes do pacote

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import ListaPacote from "./src/views/ListaPacote";
+import DetalhePacote from "./src/views/DetalhePacote";
 
 export default class App extends Component {
   render(){
     return (
-        <ListaPacote />
+        <DetalhePacote />
     );
   }
 }
--- src/views/DetalhePacote.js
+++ src/views/DetalhePacote.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, ImageBackground } from 'react-native';
+import { URL_API } from "../Api/api";
+import { Alert } from "react-native-web";
 
 export default class DetalhePacote extends Component {
+  state = {
+    detalhe: undefined,
+  };
+
+  componentDidMount(){
+    fetch(`${ URL_API }pacote/13/detalhes`)
+        .then(T => T.json())
+        .then(detalhe => this.setState({detalhe}))
+        .catch(() => Alert.alert('Error', 'nao foi possivel recuperar o pacote'))
+  }
+
   render(){
+    const {detalhe} = this.state;
+
+    if(!detalhe) {
+      return (
+          <View/>
+      )
+    }
+
     return (
         <View style={ styles.container }>
-          <Text>Open up App.js to start working on your app!</Text>
+          <ImageBackground resizeMode='cover' style={ styles.image } source={ {uri: detalhe.urlImagem} }>
+            <Text> { detalhe.pacote.nome.upperCase } </Text>
+          </ImageBackground>
+
+          <View>
+            <Text> Descrição: { detalhe.descricao }</Text>
+            <Text> Telefone: { detalhe.telefone }</Text>
+            <Text> Site: { detalhe.site }</Text>
+            <Text> Valor: { detalhe.pacote.valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}) }</Text>
+          </View>
+
+          <Button title='voltar' />
         </View>
     );
   }
@@ -18,4 +50,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: '100%',
+    height: 200,
+  }
 });
