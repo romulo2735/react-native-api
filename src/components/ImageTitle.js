eeff7129c2b77f5c3e873ba7e355ddb159260eb2
Romulo <romulosousa27@hotmail.com>
2019-10-13 20:32:13 -0300

    Aplicando estilo

--- src/components/ImageTitle.js
+++ src/components/ImageTitle.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, PixelRatio, StyleSheet, Text, View } from 'react-native';
 
 export default class ImageTitle extends Component {
   render(){
     return (
-        <View style={ styles.container }>
-          <Text>Open up App.js to start working on your app!</Text>
-        </View>
+        <ImageBackground
+            resizeMode='cover'
+            style={ styles.background }
+            source={ backgroundImageUri }>
+          <View style={ styles.container }>
+            <Text style={styles.title}>{ 'Turismo é o nosso negocio'.toUpperCase() }</Text>
+          </View>
+        </ImageBackground>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 0.5,
+    backgroundColor: '#F6DB54',
+    padding: 6,
+    borderRadius: 1,
   },
+  background: {
+    flex: 1,
+    height: 136,
+    flexDirection: 'row',
+    justifyContent: 'content',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 11 / PixelRatio.getFontScale(),
+    textAlign: 'center',
+    fontWeight: 'bold',
+  }
 });
